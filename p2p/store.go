a779d8e209f708782f1b00d087ceea27d195cfba
Revanth Madupoju <revanthmadupoju@gmail.com>
2026-04-07 00:26:04 +0500

    feat: Store and a unit-test, test successful

---
+++ p2p/store.go
@@ -0,0 +1,52 @@
+package p2p
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+type PathTransformFunc func(string) string
+
+type StoreOpts struct {
+	PathTransformFunc PathTransformFunc
+}
+
+type Store struct {
+	StoreOpts
+}
+
+var DefaultPathTransformFunc = func(key string) string {
+	return key
+}
+
+func NewStore(opts StoreOpts) *Store {
+	return &Store{
+		StoreOpts: opts,
+	}
+}
+
+func (s *Store) writeStream(key string, r io.Reader) error {
+	pathName := s.PathTransformFunc(key)
+
+	if err := os.MkdirAll(pathName, os.ModePerm); err != nil {
+		return err
+	}
+
+	filename := "someFilename"
+	pathAndFilename := pathName + "/" + filename
+
+	f, err := os.Create(pathAndFilename)
+	if err != nil {
+		return err
+	}
+
+	n, err := io.Copy(f, r)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("written (%d) bytes to disk: %s\n", n, pathAndFilename)
+
+	return nil
+}
