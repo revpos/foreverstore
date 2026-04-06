798f6ec85ec799d87975f7737324d4eb0b07b461
Revanth Madupoju <revanthmadupoju@gmail.com>
2026-04-06 23:03:41 +0500

    test: main() with early peer conn Close()

--- main.go
+++ main.go
@@ -7,11 +7,18 @@ import (
 	"github.com/revpos/foreverstore/p2p"
 )
 
+func OnPeer(p p2p.Peer) error {
+	p.Close()
+	// fmt.Println("doing some login with the peer outside of TCPTransport")
+	return nil
+}
+
 func main() {
 	tcpOpts := p2p.TCPTransportOpts{
 		ListenAddr:    ":3000",
 		HandshakeFunc: p2p.NOPHandshakeFunc,
 		Decoder:       p2p.DefaultDecoder{},
+		OnPeer: OnPeer,
 	}
 
 	tr := p2p.NewTCPTransport(tcpOpts)
